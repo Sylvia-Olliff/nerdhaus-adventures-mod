2daaf494a30e028fff1576a4871944299bf3ed38
Sylvia Olliff <sylviaolliff@gmail.com>
2025-05-11 15:46:46 -0400

    Initial Setup

---
+++ src/client/java/net/sylvanstorm/nerdhaus/adventures/NerdHausAdventuresDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sylvanstorm.nerdhaus.adventures;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NerdHausAdventuresDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
